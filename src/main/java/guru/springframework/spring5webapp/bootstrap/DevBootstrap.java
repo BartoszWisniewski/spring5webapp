14849433d84caa2970551c13a74fd50ddb6cd478
BartoszWisniewski <bartosz.wisniewski04@gmail.com>
2019-06-04 16:36:28 +0200

    Bootstrap 1

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -1,9 +1,31 @@
 package guru.springframework.spring5webapp.bootstrap;
 
-public class DevBootstrap implements  {
+import guru.springframework.spring5webapp.model.Author;
+import guru.springframework.spring5webapp.model.Book;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
 
     private void initData(){
-        
+
+        //Eric
+        Author eric = new Author("Eric" , "Evans");
+        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        //Rod
+        Author rod = new Author("Rod" , "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+        rod.getBooks().add(noEJB);
+
     }
 
 }
